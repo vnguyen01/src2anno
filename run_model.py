b6eb60f58c0aad153813eef46276e92a1e51fab5
Vincent Nguyen <vnguyen01@college.harvard.edu>
2016-04-21 14:50:54 -0400

    check

--- run_model.py
+++ run_model.py
@@ -1,15 +1,36 @@
 import sys
 import os
 
-def run_seq2seq_attn(ftrain, fvalid, fmodel, ftest, fpredict):
+FILE_PATHS = {"train" : "data/en-django/django-train.hdf5",
+			  "valid" : "data/en-django/django-val.hdf5",
+			  "test" : "data/en-django/src-val.txt",
+			  "dic_src" : "data/en-django/django.src.dict",
+			  "dic_tar" : "data/en-django/django.tar.dict"}
+
+def run_seq2seq_attn(ftrain, fvalid, fmodel, ftest, fpredict, dsrc, dtar):
 	os.chdir('seq2seq_attn')
+
+	#train
 	os.system('th train.lua -data_file ' +\
 	 ftrain + ' -val_data_file ' + \
 	 fvalid + ' -savefile ' + fmodel)
-	os.system('th beam.lua -model ' + fmodel + ' -src_file ' + ftest + ' -out_file' + fpredict)
+
+	#predict
+	os.system('th beam.lua -model ' + fmodel +\
+	 ' -src_file ' + ftest + ' -output_file' + fpredict +\
+	 ' -src_dict ' + dsrc + ' -targ_dict ' + dtar)
+
 
 def main():
-	run_seq2seq_attn(ftrain, fvalid, fmodel, ftest, fpredict)
+	ftrain = FILE_PATHS["train"]
+	fvalid = FILE_PATHS["valid"]
+	ftest = FILE_PATHS["test"]
+	fpredict = "data/en-django/pred.txt"
+
+	dsrc = FILE_PATHS["dic_src"]
+	dtar = FILE_PATHS["dic_tar"]
+
+	run_seq2seq_attn(ftrain, fvalid, fmodel, ftest, fpredict, dsrc, dtar)
 
 if __name__ == "__main__":
 	sys.exit(main())
