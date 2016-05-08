38dbc3f10070f9b0592baa5a01e922ebb396f685
Vincent Nguyen <vnguyen01@college.harvard.edu>
2016-05-08 01:17:14 -0400

    ready to run on cluster

--- run_model.py
+++ run_model.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
-FILE_PATHS = {"train" : "data/en-django/django3-train.hdf5",
-			  "valid" : "data/en-django/django3-val.hdf5",
-			  "test" : "data/en-django/src-val3.txt",
-			  "model" : "django-model3",
-			  "dic_src" : "data/en-django/django3.src.dict",
-			  "dic_tar" : "data/en-django/django3.tar.dict"}
+FILE_PATHS = {"train" : "data/en-django/django-train.hdf5",
+			  "valid" : "data/en-django/django-val.hdf5",
+			  "test" : "data/en-django/src-val.txt",
+			  "model" : "django-model",
+			  "dic_src" : "data/en-django/django.src.dict",
+			  "dic_tar" : "data/en-django/django.tar.dict"}
 
 def run_seq2seq_attn(ftrain, fvalid, fmodel, ftest, fpredict, dsrc, dtar):
 	os.chdir('seq2seq-attn')
@@ -14,19 +14,18 @@ def run_seq2seq_attn(ftrain, fvalid, fmodel, ftest, fpredict, dsrc, dtar):
 	os.system('th train.lua -data_file ' +\
 	 "../" + ftrain + ' -val_data_file ' + \
 	 "../" + fvalid + ' -savefile ' + fmodel +\
-	  " -epochs 20 -num_layers 2 -word_vec_size 200 -rnn_size 200 -dropout 0.5 -gpuid -1")
+	  " -epochs 50 -num_layers 3 -word_vec_size 500 -rnn_size 500 -dropout 0.5")
 	
-	#os.system('th beam.lua -model ' + fmodel +\
-	# ' -src_file ' + ftest + ' -output_file' + fpredict +\
-	# ' -src_dict ' + dsrc + ' -targ_dict ' + dtar)
 	
+	os.system('th beam.lua -model django-model_final.t7 -src_file ../data/en-django/src-val.txt -output_file pred.txt -src_dict ../data/en-django/django.src.dict -targ_dict ../data/en-django/django.targ.dict -max_sent_l 500')
+
 
 def main():
 	ftrain = FILE_PATHS["train"]
 	fvalid = FILE_PATHS["valid"]
 	fmodel = FILE_PATHS["model"]
 	ftest = FILE_PATHS["test"]
-	fpredict = "data/en-django/pred3.txt"
+	fpredict = "pred.txt"
 
 	dsrc = FILE_PATHS["dic_src"]
 	dtar = FILE_PATHS["dic_tar"]
